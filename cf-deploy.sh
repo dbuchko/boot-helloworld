f39fd5ead2525a39c2ecad6c62d8473f87cd7e38
dbuchko <dbuchko@vmware.com>
2025-05-29 14:16:42 -0400

    polish

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -5,5 +5,3 @@ cf create-service app-autoscaler standard hello-autoscaler
 cf push dlb-hello
 cf configure-autoscaling dlb-hello autoscaler.yml
 cf enable-autoscaling dlb-hello
-
-cf start dlb-hello
